02c3cc33376aa399f248c4a29ff9b3c27d541891
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2023-11-12 15:03:08 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,12 @@
 import "./App.css";
 import React, { useState, useContext, useEffect } from "react";
-import Form from "./form";
-import Google from "./Google";
-// import Messages from "./Messages";
-import Msg from "./msgcomp/Msg";
-import Storage from "./Storage";
 import {Route,Link, BrowserRouter as Router, Switch} from 'react-router-dom'
-import Routes from "./router/route";
-import Another from "./router/another";
-import Frame from "./router/frame";
-const Home = () =>{
-  return(
-    <>
-    <nav className="flex  gap-4 items-center justify-center">
-    <Link to="/" >Home</Link>
-     <Link to="/route" >Route</Link>
-     <Link to="/another" >Another</Link>
-    </nav>
-    </>
-  )
-}
+
 const App = () => {
-  // const {name,setName,signIn,view,img} = useGlobal();
-  const getSomething = () =>{
-    return new Promise((resolve,reject) =>{
-      resolve('https://api.github.com/users')
-    })
-  }
-  getSomething().then((data) =>{
-    console.log(data);
-  },(err)=>{
-    console.log(err);
-  })
-  return (
-<Router>
-<div className=" flex flex-col items-center ">
-      {/* {signIn && <Google />}
-      {view && <Messages />}  */}
-      {/*<Storage/>*/}
-      <Home/>
-      <Switch>
-        <Route exact path="/" >
-        <h1>Hello world </h1>
-        </Route>
-        <Route path="/route" >
-          <Routes/>
-        </Route>
-        <Route  path="/another" >
-          <Frame/>
-        </Route>
-      </Switch>
-    </div>
-</Router>
-  );
+ return(
+   <h1>Hello world</h1>
+  )
+  
 };
 
 export default App;
--- src/context.jsx
+++ src/context.jsx
@@ -1,17 +1,10 @@
 import React, { useContext, useState, useEffect } from "react";
 const AppContext = React.createContext();
 import { motion, AnimatePresence } from "framer-motion";
-import money from './assets/money.png'
 function AppProvider({ children }) {
- const [name,setName] = useState("User101");
-  const [signIn, setSignIn] = useState(false);
-  const [view, setView] = useState(true);
-//  const [img, setImg] = useState(
-//    "https://lh3.googleusercontent.com/a/ACg8ocI-XIxka7VH209b-UB9U9lPqA2g-omrmvg_211bWyYUCw=s96-c"
-//  );
- const [img, setImg] = useState(money);
+ 
   return (
-    <AppContext.Provider value={{img,setImg,signIn,view,name,setName}} >
+    <AppContext.Provider value="hello" >
       {children}
     </AppContext.Provider>
   );
